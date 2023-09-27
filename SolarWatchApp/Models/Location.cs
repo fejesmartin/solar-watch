c041722c1959947e0f11cd44d99412a39ceb5cca
fejesmartin <fejes.martin00@gmail.com>
2023-09-27 16:28:40 +0200

    feature: adds Models

---
+++ SolarWatchApp/Models/Location.cs
@@ -0,0 +1,13 @@
+﻿namespace SolarWatchApp.Models;
+
+public class Location
+{
+    public double Lat { get; set; }
+    public double Lng { get; set; }
+
+    public Location(double lat, double lng)
+    {
+        Lat = lat;
+        Lng = lng;
+    }
+}
