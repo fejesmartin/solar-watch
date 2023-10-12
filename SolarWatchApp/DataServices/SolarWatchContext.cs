2444f8db8560d658336aa140f8ab3a6e9cf81813
fejesmartin <fejes.martin00@gmail.com>
2023-10-12 14:26:27 +0200

    feature: adds SolarWatch Context class

---
+++ SolarWatchApp/DataServices/SolarWatchContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using SolarWatchApp.Models;
+
+namespace SolarWatchApp.DataServices;
+
+public class SolarWatchContext: DbContext
+{
+    public DbSet<City> Cities { get; set; }
+    public DbSet<SunsetSunrise> SunsetSunrises { get; set; }
+
+    public SolarWatchContext(DbContextOptions<SolarWatchContext> options): base(options)
+    {
+        
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("ASPNETCORE_CONNECTIONSTRING");
+        
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+        modelBuilder.Entity<City>()
+            .HasOne(city => city.SunsetSunrise)
+            .WithOne(sunrise => sunrise.City)
+            .HasForeignKey<SunsetSunrise>(sunrise => sunrise.CityId);
+    }
+}
