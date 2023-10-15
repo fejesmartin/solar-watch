1877c10f333c60fb02c722c1fcf2177335a8bf39
fejesmartin <fejes.martin00@gmail.com>
2023-10-16 00:52:20 +0200

    feature: extended and fixed json processor

--- SolarWatchApp/JsonProcessor/IJsonProcessor.cs
+++ SolarWatchApp/JsonProcessor/IJsonProcessor.cs
@@ -1,7 +1,13 @@
-﻿namespace SolarWatchApp.JsonProcessor;
+﻿using System.Text.Json;
+
+namespace SolarWatchApp.JsonProcessor;
 
 public interface IJsonProcessor
 {
+    public string GetStringPropertySS(string json, string propertyName);
+    public double GetDoublePropertySS(string json, string propertyName);
+    public string GetStringPropertyGeo(string json, string propertyName);
+    public double GetDoublePropertyGeo(string json, string propertyName);
     public string GetStringProperty(string json, string propertyName);
     public double GetDoubleProperty(string json, string propertyName);
 }
