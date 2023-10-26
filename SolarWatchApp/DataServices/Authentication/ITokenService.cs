5113124d3b13acc486fdba28c118627d518d42f0
fejesmartin <fejes.martin00@gmail.com>
2023-10-26 15:58:09 +0200

    feature: implements TokenService

---
+++ SolarWatchApp/DataServices/Authentication/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SolarWatchApp.DataServices.Authentication;
+
+public interface ITokenService
+{
+    public string CreateToken(IdentityUser user);
+}
