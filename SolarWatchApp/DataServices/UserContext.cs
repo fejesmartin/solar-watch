e336a3ef9d9396461906ebf482008c7371d9761d
fejesmartin <fejes.martin00@gmail.com>
2023-10-26 15:58:22 +0200

    feature: implements UserContext

---
+++ SolarWatchApp/DataServices/UserContext.cs
@@ -0,0 +1,25 @@
+﻿using DotNetEnv;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SolarWatchApp.DataServices;
+
+public class UserContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+{
+    public UserContext (DbContextOptions<UserContext> options) : base(options)
+    {
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        Env.Load();
+        var connectionString = Environment.GetEnvironmentVariable("ASPNETCORE_CONNECTIONSTRING");
+        options.UseSqlServer(connectionString);
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
