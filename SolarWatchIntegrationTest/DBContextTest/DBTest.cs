2c49eb18f763c8be2d9644c02fb16be759f4eedb
fejesmartin <fejes.martin00@gmail.com>
2023-11-01 00:06:11 +0100

    feature: implements DBTest for testing DB Context

---
+++ SolarWatchIntegrationTest/DBContextTest/DBTest.cs
@@ -0,0 +1,19 @@
+﻿using SolarWatchApp.DataServices;
+using Microsoft.EntityFrameworkCore;
+
+namespace SolarWatchIntegrationTest.DBContextTest
+{
+    public sealed class DBTest : SolarWatchContext
+    {
+        public DBTest(DbContextOptions<SolarWatchContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseInMemoryDatabase(databaseName: "DBTest");
+        }
+    }
+}
